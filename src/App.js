aa04d42441576f6fe8b11baf1df1fd5685bb5753
helllth <th@cismet.de>
2021-03-16 08:40:24 +0100

    added the feature collection and a lot of helkper functions

--- src/App.js
+++ src/App.js
@@ -6,38 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "leaflet/dist/leaflet.css";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import "react-cismap/topicMaps.css";
-import { md5FetchText, fetchJSON } from "react-cismap/tools/fetching";
-import { getGazDataForTopicIds } from "react-cismap/tools/gazetteerHelper";
-
 import TopicMapContextProvider from "react-cismap/contexts/TopicMapContextProvider";
-import { getClusterIconCreatorFunction } from "react-cismap/tools/uiHelper";
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 import FeatureCollection from "react-cismap/FeatureCollection";
 import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
+
 import getGTMFeatureStyler from "react-cismap/topicmaps/generic/GTMStyler";
 import ContactButton from "react-cismap/ContactButton";
-const host = "https://wupp-topicmaps-data.cismet.de";
-
-const getGazData = async (setGazData) => {
-  const prefix = "GazDataForStories";
-  const sources = {};
-
-  sources.adressen = await md5FetchText(prefix, host + "/data/adressen.json");
-  sources.bezirke = await md5FetchText(prefix, host + "/data/bezirke.json");
-  sources.quartiere = await md5FetchText(prefix, host + "/data/quartiere.json");
-  sources.pois = await md5FetchText(prefix, host + "/data/pois.json");
-  sources.kitas = await md5FetchText(prefix, host + "/data/kitas.json");
-
-  const gazData = getGazDataForTopicIds(sources, [
-    "pois",
-    "kitas",
-    "bezirke",
-    "quartiere",
-    "adressen",
-  ]);
-
-  setGazData(gazData);
-};
+import { getGazData, convertItemToFeature, LogSelection } from "./helper";
+import { getClusterIconCreatorFunction } from "react-cismap/tools/uiHelper";
 
 function App() {
   const [gazData, setGazData] = useState([]);
@@ -46,14 +23,44 @@ function App() {
     document.title = "Luftmessstationenskarte Wuppertal";
   }, []);
   return (
-    <TopicMapContextProvider appKey='LuftmessstationenWuppertal.TopicMap'>
-      <TopicMapComponent locatorControl={true} gazData={gazData}>
+    <TopicMapContextProvider
+      appKey='LuftmessstationenWuppertal.TopicMap'
+      featureItemsURL={"https://wupp-topicmaps-data.cismet.de/data/no2.data.json"}
+      getFeatureStyler={getGTMFeatureStyler}
+      featureTooltipFunction={(feature) => feature?.text}
+      convertItemToFeature={convertItemToFeature}
+      clusteringOptions={{
+        iconCreateFunction: getClusterIconCreatorFunction(30, (props) => props.color),
+      }}
+    >
+      <TopicMapComponent
+        locatorControl={true}
+        gazData={gazData}
+        infoBox={
+          <GenericInfoBoxFromFeature
+            pixelwidth={350}
+            config={{
+              city: "Wuppertal",
+              navigator: {
+                noun: {
+                  singular: "Messstation",
+                  plural: "Messstationen",
+                },
+              },
+              noCurrentFeatureTitle: "Keine Messtationen gefunden",
+              noCurrentFeatureContent: "",
+            }}
+          />
+        }
+      >
         <ContactButton
           title='Cooltip ;-)'
           action={() => {
             window.alert("Rückfragemöglichkeit zu den Messwerten");
           }}
         />
+        <FeatureCollection></FeatureCollection>
+        <LogSelection />
       </TopicMapComponent>
     </TopicMapContextProvider>
   );
---
+++ src/helper.js
@@ -0,0 +1,258 @@
+import { addSVGToProps } from "react-cismap/tools/svgHelper";
+import { md5FetchText, fetchJSON } from "react-cismap/tools/fetching";
+import { getGazDataForTopicIds } from "react-cismap/tools/gazetteerHelper";
+import { useContext } from "react";
+import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
+
+const host = "https://wupp-topicmaps-data.cismet.de";
+
+export const LOOKUP = {
+  abgebaut: {
+    color: "#9DBCCC",
+    signature: "Luft_Icon_Messstation_abgebaut_farbig.svg",
+    header: "Messstation für NO² (inaktiv, demontiert)",
+  },
+  inaktiv: {
+    color: "#4AFAFC5",
+    signature: "Luft_Icon_Messstation_deaktiv_farbig.svg",
+    header: "Messstation für NO² (aktiv, aktuell Messausfall)",
+  },
+  unauffaellig: {
+    color: "#9ACD32",
+    signature: "Luft_Icon_Messstation_unauffaellig_farbig.svg",
+    header: "Messstation für NO² (aktiv, unauffällig)",
+  },
+  auffaellig: {
+    color: "#FFA500",
+    signature: "Luft_Icon_Messstation_auffaellig_farbig.svg",
+    header: "Messstation für NO² (aktiv, auffällig)",
+  },
+  warnend: {
+    color: "#E01414",
+    signature: "Luft_Icon_Messstation_warnend_farbig.svg",
+    header: "Messstation für NO² (aktiv, warnend)",
+  },
+  unknown: { color: "#eeeeee", signature: "Platz.svg", header: "Fehler" },
+};
+
+const MONTHS = [
+  "Januar",
+  "Februar",
+  "März",
+  "April",
+  "Mai",
+  "Juni",
+  "Juli",
+  "August",
+  "September",
+  "Oktober",
+  "November",
+  "Dezember",
+];
+
+export const getGazData = async (setGazData) => {
+  const prefix = "GazDataForStories";
+  const sources = {};
+
+  sources.adressen = await md5FetchText(prefix, host + "/data/adressen.json");
+  sources.bezirke = await md5FetchText(prefix, host + "/data/bezirke.json");
+  sources.quartiere = await md5FetchText(prefix, host + "/data/quartiere.json");
+  sources.pois = await md5FetchText(prefix, host + "/data/pois.json");
+  sources.kitas = await md5FetchText(prefix, host + "/data/kitas.json");
+
+  const gazData = getGazDataForTopicIds(sources, [
+    "pois",
+    "kitas",
+    "bezirke",
+    "quartiere",
+    "adressen",
+  ]);
+
+  setGazData(gazData);
+};
+
+const hasValues = (item) => {
+  const values = item?.werte;
+  return values !== undefined && JSON.stringify(values) !== "{}";
+};
+
+const getLastMeasurement = (item) => {
+  const lym = getLastYearMeasurements(item);
+
+  if (lym) {
+    const { values: lymValues, year } = lym;
+    if (lymValues.length === 13) {
+      return { value: lymValues[11], monthIndex: 11, year };
+    } else {
+      return { value: lymValues[lymValues.length - 1], monthIndex: lymValues.length - 1, year };
+    }
+  }
+};
+const getLastYear = (item) => {
+  if (hasValues(item)) {
+    const values = item?.werte;
+    const sortedkeys = Object.keys(values).sort();
+    return sortedkeys[sortedkeys.length - 1];
+  }
+};
+const getLastYearM1 = (item) => {
+  if (hasValues(item)) {
+    const values = item?.werte;
+    const sortedkeys = Object.keys(values).sort();
+    if (sortedkeys.length > 1) {
+      return sortedkeys[sortedkeys.length - 2];
+    }
+  }
+};
+
+const getLastYearMeasurements = (item) => {
+  const ly = getLastYear(item);
+  const values = item?.werte;
+  if (ly) {
+    return { values: values[ly], year: ly };
+  }
+};
+const getLastYearMinus1Measurements = (item) => {
+  const lym1 = getLastYearM1(item);
+  const values = item?.werte;
+  if (lym1) {
+    return values[lym1];
+  }
+};
+
+const getWeightedAverage = (measurments) => {
+  if (measurments && measurments.length === 13) {
+    return measurments[12];
+  }
+};
+const getActivityStatus = (item) => {
+  if (item?.bis !== undefined) {
+    return "abgebaut";
+  } else {
+    return "aktiv";
+  }
+  //todo inaktiv
+};
+
+// unauffällig 1-35 (einschließlich) µg/m³ Grün
+// auffällig 36-40 (einschließlich) µg/m³ Orange
+// warnend >40 µg/m³ Rot
+const getStatus = (item) => {
+  if (getActivityStatus(item) === "aktiv") {
+    const lym = getLastMeasurement(item)?.value;
+    console.log("lym", lym);
+
+    if (lym > 0 && lym <= 35) {
+      return "unauffaellig";
+    } else if (lym > 35 && lym <= 40) {
+      return "auffaellig";
+    } else if (lym > 40) {
+      return "warnend";
+    } else if (lym === 9999) {
+      return "inaktiv";
+    } else {
+      return "unknown";
+    }
+  } else {
+    return getActivityStatus(item);
+  }
+};
+
+const getSignature = (item) => {
+  const status = getStatus(item);
+  return LOOKUP[status].signature;
+};
+
+export const LogSelection = () => {
+  const { selectedFeature } = useContext(FeatureCollectionContext);
+  console.log("selectedFeature.properties", selectedFeature?.properties);
+
+  return <div></div>;
+};
+
+const getAdditionalInfo = (item) => {
+  const lastYearAverage = getWeightedAverage(getLastYearMeasurements(item)?.values);
+  const lastYear = getLastYear(item);
+  const lastYearM1Average = getWeightedAverage(getLastYearMinus1Measurements(item));
+  const lastYearM1 = getLastYearM1(item);
+
+  let ret = "";
+  if (lastYearAverage || lastYearM1Average) {
+    ret = "gewichtete Jahresmittelwerte:";
+    if (lastYearAverage) {
+      ret = ret + "\n" + lastYearAverage + " µg/m³ (" + lastYear + ")";
+    }
+    if (lastYearM1Average) {
+      ret = ret + "\n" + lastYearM1Average + " µg/m³ (" + lastYearM1 + ")";
+    }
+  }
+  console.log("ret", ret);
+
+  return ret;
+};
+
+const getTitle = (item) => {
+  const lm = getLastMeasurement(item);
+
+  if (item?.bis) {
+    const demontage = new Date(item?.bis);
+    console.log("demontiert", demontage.getMonth(), demontage.getFullYear());
+
+    return `Diese Messstation ist seit ${
+      MONTHS[demontage.getMonth() - 1]
+    } ${demontage.getFullYear()} abmontiert`;
+  } else {
+    if (lm) {
+      if (lm.value !== 9999) {
+        return lm.value + " µg/m³ (" + MONTHS[lm.monthIndex] + " " + lm.year + ")";
+      } else {
+        return (
+          "Für " +
+          MONTHS[lm.monthIndex] +
+          " " +
+          lm.year +
+          "liefert diese Messstation keinen Messwert"
+        );
+      }
+    } else {
+      return "Diese Messstation ist abmontiert";
+    }
+  }
+};
+
+export const convertItemToFeature = async (itemIn) => {
+  //   console.log("itemIn", itemIn);
+
+  let item = await addSVGToProps(itemIn, (i) => "luft/" + getSignature(i));
+  item.status = getStatus(item);
+  const text = item?.name || "Kein Namexx";
+  const type = "Feature";
+  const selected = false;
+  const geometry = item?.geojson;
+  item.color = LOOKUP[item.status].color;
+  const info = {
+    header: LOOKUP[item.status].header,
+    title: getTitle(item),
+    additionalInfo: getAdditionalInfo(item),
+    subtitle: (
+      <span>
+        {item?.strasse} {item?.hausnummer} {item?.zusatzinfo && <span>({item?.zusatzinfo})</span>}
+      </span>
+    ),
+  };
+  item.info = info;
+  item.foto = "https://www.wuppertal.de/geoportal/luftmessstationen/fotos/NO2_test.jpg";
+  return {
+    text,
+    type,
+    selected,
+    geometry,
+    crs: {
+      type: "name",
+      properties: {
+        name: "urn:ogc:def:crs:EPSG::25832",
+      },
+    },
+    properties: item,
+  };
+};
